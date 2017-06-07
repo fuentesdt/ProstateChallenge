6de7814f56eeda592ec521139f1aac5d1242d2cf
David Fuentes <fuentesdt@gmail.com>
2017-06-07 09:44:42 -0500

    WIP: register

--- prostatechallenge.makefile
+++ prostatechallenge.makefile
@@ -15,18 +15,30 @@ dependencies: ./prostatechallenge.sql
 config: $(addprefix $(WORKDIR)/,$(addsuffix /config,$(TRAINING)))  
 viewdata: $(addprefix $(WORKDIR)/,$(addsuffix /viewdata,$(TRAINING)))  
 truth: $(addprefix $(WORKDIR)/,$(addsuffix /TRUTH.nii.gz,$(TRAINING)))  
-reg: $(addprefix $(WORKDIR)/,$(addsuffix /T2Sag.reg.nii.gz,$(TRAINING)))  $(addprefix $(WORKDIR)/,$(addsuffix /ADC.reg.nii.gz,$(TRAINING)))  $(addprefix $(WORKDIR)/,$(addsuffix /BVAL.reg.nii.gz,$(TRAINING)))  $(addprefix $(WORKDIR)/,$(addsuffix /KTRANS.reg.nii.gz,$(TRAINING)))  
+lmpre: $(addprefix $(WORKDIR)/,$(addsuffix /landmarks.1.txt,$(TRAINING)))   \
+       $(addprefix $(WORKDIR)/,$(addsuffix /landmarks.2.txt,$(TRAINING)))   \
+       $(addprefix $(WORKDIR)/,$(addsuffix /landmarks.3.txt,$(TRAINING)))   \
+       $(addprefix $(WORKDIR)/,$(addsuffix /landmarks.4.txt,$(TRAINING)))    
+lm: $(addprefix $(WORKDIR)/,$(addsuffix /landmarks.txt,$(TRAINING)))  
+reslice: $(addprefix $(WORKDIR)/,$(addsuffix /T2Sag.reslice.nii.gz,$(TRAINING)))  $(addprefix $(WORKDIR)/,$(addsuffix /ADC.reslice.nii.gz,$(TRAINING)))  $(addprefix $(WORKDIR)/,$(addsuffix /BVAL.reslice.nii.gz,$(TRAINING)))  $(addprefix $(WORKDIR)/,$(addsuffix /KTRANS.reslice.nii.gz,$(TRAINING)))  
+sform: $(addprefix $(WORKDIR)/,$(addsuffix /T2Axial.sform.nii.gz,$(TRAINING))) $(addprefix $(WORKDIR)/,$(addsuffix /T2Sag.sform.nii.gz,$(TRAINING)))  $(addprefix $(WORKDIR)/,$(addsuffix /ADC.sform.nii.gz,$(TRAINING)))  $(addprefix $(WORKDIR)/,$(addsuffix /BVAL.sform.nii.gz,$(TRAINING)))  
 
-$(WORKDIR)/%/TRUTH.nii.gz: $(WORKDIR)/%/landmarks.txt $(WORKDIR)/%/T2Axial.nii.gz
+$(WORKDIR)/%/landmarks.txt: 
+	cat $(WORKDIR)/$*/landmarks.?.txt > $@ 
+$(WORKDIR)/%/TRUTH.nii.gz: $(WORKDIR)/%/landmarks.txt $(WORKDIR)/%/T2Axial.sform.nii.gz
 	$(C3DEXE) $(word 2,$^) -scale 0 -lts $< 5 -o $@
 
-$(WORKDIR)/%.reg.nii.gz: $(WORKDIR)/%.raw.nii.gz
+$(WORKDIR)/%.sform.nii.gz: $(WORKDIR)/%.raw.nii.gz $(WORKDIR)/%.world
+	sed 's/,/ /g' $(word 2,$^) > $(basename $(word 2,$^)).mat
+	$(C3DEXE)  $< -set-sform $(basename $(word 2,$^)).mat -o $@
+
+$(WORKDIR)/%.reslice.nii.gz: $(WORKDIR)/%.raw.nii.gz
 	$(C3DEXE) $(@D)/T2Axial.raw.nii.gz  $< -reslice-identity -o $@
 
 $(WORKDIR)/%/viewdata:
-	$(C3DEXE) $(@D)/T2Axial.raw.nii.gz -info
-	$(C3DEXE) $(@D)/T2Sag.raw.nii.gz   -info 
-	$(C3DEXE) $(@D)/ADC.raw.nii.gz     -info
-	$(C3DEXE) $(@D)/BVAL.raw.nii.gz    -info
+	$(C3DEXE) $(@D)/T2Axial.sform.nii.gz -info
+	$(C3DEXE) $(@D)/T2Sag.sform.nii.gz   -info 
+	$(C3DEXE) $(@D)/ADC.sform.nii.gz     -info
+	$(C3DEXE) $(@D)/BVAL.sform.nii.gz    -info
 	$(C3DEXE) $(@D)/KTRANS.raw.nii.gz  -info
-	$(ITKSNAP) -g $(@D)/T2Axial.raw.nii.gz -s $(@D)/TRUTH.nii.gz -o $(@D)/T2Sag.reg.nii.gz   $(@D)/ADC.reg.nii.gz     $(@D)/BVAL.reg.nii.gz    $(@D)/KTRANS.reg.nii.gz  
+	$(ITKSNAP) -g $(@D)/T2Axial.sform.nii.gz -s $(@D)/TRUTH.nii.gz -o $(@D)/T2Sag.reslice.nii.gz   $(@D)/ADC.reslice.nii.gz     $(@D)/BVAL.reslice.nii.gz    $(@D)/KTRANS.reslice.nii.gz  
--- prostatechallenge.sql
+++ prostatechallenge.sql
@@ -15,28 +15,44 @@ BEGIN
   CREATE TABLE DFProstateChallenge.metadata(
   id    bigint(20) NOT NULL AUTO_INCREMENT,
   mrn                 VARCHAR(32)          not null COMMENT 'PT UID'     ,
+  fid                 VARCHAR(256)         not NULL  COMMENT 'findings' ,
+  pos                 VARCHAR(256)         not NULL  COMMENT 'findings' ,
   T2AxialUID          VARCHAR(256)         not NULL  COMMENT 'series UID' ,
   T2SagUID            VARCHAR(256)         not NULL  COMMENT 'series UID' ,
   ADCUID              VARCHAR(256)         not NULL  COMMENT 'series UID' ,
   BVALUID             VARCHAR(256)         not NULL  COMMENT 'series UID' ,
+  T2AxialWorld        VARCHAR(256)         not NULL  COMMENT 'world matrix' ,
+  T2SagWorld          VARCHAR(256)         not NULL  COMMENT 'world matrix' ,
+  ADCWorld            VARCHAR(256)         not NULL  COMMENT 'world matrix' ,
+  BVALWorld           VARCHAR(256)         not NULL  COMMENT 'world matrix' ,
+  T2AxialIJK          VARCHAR(256)         not NULL  COMMENT 'IJK Coord' ,
+  T2SagIJK            VARCHAR(256)         not NULL  COMMENT 'IJK Coord' ,
+  ADCIJK              VARCHAR(256)         not NULL  COMMENT 'IJK Coord' ,
+  BVALIJK             VARCHAR(256)         not NULL  COMMENT 'IJK Coord' ,
   KTRANSUID           VARCHAR(256)         GENERATED ALWAYS AS (concat(mrn ,'-Ktrans.mhd') ) COMMENT 'ktrans',
-  fid                 VARCHAR(256)         not NULL  COMMENT 'findings' ,
-  pos                 VARCHAR(256)         not NULL  COMMENT 'findings' ,
-  ggg                         int              NULL  COMMENT 'gleason' ,
+  ggg                         int              NULL  COMMENT 'gleason group' ,
   PRIMARY KEY (id),
   KEY `UID1` (`mrn`) 
   );
-  insert into DFProstateChallenge.metadata( mrn                 , T2AxialUID          , T2SagUID            , ADCUID              , BVALUID             , fid                 , pos                 )
+  insert into DFProstateChallenge.metadata( mrn, T2AxialUID, T2SagUID, ADCUID, BVALUID, T2AxialWorld, T2SagWorld, ADCWorld, BVALWorld, T2AxialIJK, T2SagIJK, ADCIJK, BVALIJK, fid, pos)
   SELECT JSON_UNQUOTE(eu.data->"$.""ProxID""") "ProxID",
          GROUP_CONCAT( distinct CASE WHEN JSON_UNQUOTE(eu.data->"$.""Name""") like "%t2%tra%" THEN  JSON_UNQUOTE(eu.data->"$.""DCMSerUID""") END ) T2AxialUID, 
          GROUP_CONCAT( distinct CASE WHEN JSON_UNQUOTE(eu.data->"$.""Name""") like "%t2%sag%" THEN  JSON_UNQUOTE(eu.data->"$.""DCMSerUID""") END ) T2SagUID,
          GROUP_CONCAT( distinct CASE WHEN JSON_UNQUOTE(eu.data->"$.""Name""") like "%ADC%"    THEN  JSON_UNQUOTE(eu.data->"$.""DCMSerUID""") END ) ADCUID,
          GROUP_CONCAT( distinct CASE WHEN JSON_UNQUOTE(eu.data->"$.""Name""") like "%BVAL%"   THEN  JSON_UNQUOTE(eu.data->"$.""DCMSerUID""") END ) BVALUID,
+         GROUP_CONCAT( distinct CASE WHEN JSON_UNQUOTE(eu.data->"$.""Name""") like "%t2%tra%" THEN  JSON_UNQUOTE(eu.data->"$.""WorldMatrix""") END ) T2AxialWorld, 
+         GROUP_CONCAT( distinct CASE WHEN JSON_UNQUOTE(eu.data->"$.""Name""") like "%t2%sag%" THEN  JSON_UNQUOTE(eu.data->"$.""WorldMatrix""") END ) T2SagWorld,
+         GROUP_CONCAT( distinct CASE WHEN JSON_UNQUOTE(eu.data->"$.""Name""") like "%ADC%"    THEN  JSON_UNQUOTE(eu.data->"$.""WorldMatrix""") END ) ADCWorld,
+         GROUP_CONCAT( distinct CASE WHEN JSON_UNQUOTE(eu.data->"$.""Name""") like "%BVAL%"   THEN  JSON_UNQUOTE(eu.data->"$.""WorldMatrix""") END ) BVALWorld,
+         GROUP_CONCAT( distinct CASE WHEN JSON_UNQUOTE(eu.data->"$.""Name""") like "%t2%tra%" THEN  JSON_UNQUOTE(eu.data->"$.""ijk""") END ) T2AxialIJK, 
+         GROUP_CONCAT( distinct CASE WHEN JSON_UNQUOTE(eu.data->"$.""Name""") like "%t2%sag%" THEN  JSON_UNQUOTE(eu.data->"$.""ijk""") END ) T2SagIJK,
+         GROUP_CONCAT( distinct CASE WHEN JSON_UNQUOTE(eu.data->"$.""Name""") like "%ADC%"    THEN  JSON_UNQUOTE(eu.data->"$.""ijk""") END ) ADCIJK,
+         GROUP_CONCAT( distinct CASE WHEN JSON_UNQUOTE(eu.data->"$.""Name""") like "%BVAL%"   THEN  JSON_UNQUOTE(eu.data->"$.""ijk""") END ) BVALIJK,
          JSON_UNQUOTE(eu.data->"$.""fid""") "fid",
          JSON_UNQUOTE(eu.data->"$.""pos""") "pos" 
   FROM ClinicalStudies.excelUpload eu 
   where eu.uploadID = 48
-  group by JSON_UNQUOTE(eu.data->"$.""ProxID""") ;
+  group by JSON_UNQUOTE(eu.data->"$.""ProxID""") , JSON_UNQUOTE(eu.data->"$.""fid""");
 
   update DFProstateChallenge.metadata md
     join ClinicalStudies.excelUpload  eu on eu.uploadID = 49 and  md.mrn= JSON_UNQUOTE(eu.data->"$.""ProxID""")  
@@ -63,17 +79,24 @@ BEGIN
     select CONCAT('Processed/',rf.mrn,'/config:',' Processed/',rf.mrn,'/T2Axial.raw.nii.gz',' Processed/',rf.mrn,'/T2Sag.raw.nii.gz' , ' Processed/', rf.mrn, '/ADC.raw.nii.gz' , ' Processed/', rf.mrn, '/BVAL.raw.nii.gz' , ' Processed/', rf.mrn, '/KTRANS.raw.nii.gz' ) 
     from DFProstateChallenge.metadata rf ;
     select CONCAT('Processed/', rf.mrn, '/T2Axial.raw.nii.gz:\n\tmkdir -p $(@D); DicomSeriesReadImageWrite2  /rsrch1/ip/dtfuentes/PROSTATExChallenge2/ProstateTrain/DOI/', rf.mrn,'/*/', rf.T2AxialUID , ' $@' ) 
-    from DFProstateChallenge.metadata rf ;
+    from DFProstateChallenge.metadata rf group by rf.mrn;
     select CONCAT('Processed/', rf.mrn, '/T2Sag.raw.nii.gz:\n\tmkdir -p $(@D); DicomSeriesReadImageWrite2  /rsrch1/ip/dtfuentes/PROSTATExChallenge2/ProstateTrain/DOI/'  , rf.mrn,'/*/', rf.T2SagUID   , ' $@' ) 
-    from DFProstateChallenge.metadata rf ;
+    from DFProstateChallenge.metadata rf group by rf.mrn;
     select CONCAT('Processed/', rf.mrn, '/ADC.raw.nii.gz:\n\tmkdir -p $(@D); DicomSeriesReadImageWrite2  /rsrch1/ip/dtfuentes/PROSTATExChallenge2/ProstateTrain/DOI/'    , rf.mrn,'/*/', rf.ADCUID     , ' $@' ) 
-    from DFProstateChallenge.metadata rf ;
+    from DFProstateChallenge.metadata rf group by rf.mrn;
     select CONCAT('Processed/', rf.mrn, '/BVAL.raw.nii.gz:\n\tmkdir -p $(@D); DicomSeriesReadImageWrite2  /rsrch1/ip/dtfuentes/PROSTATExChallenge2/ProstateTrain/DOI/'   , rf.mrn,'/*/', rf.BVALUID    , ' $@' ) 
-    from DFProstateChallenge.metadata rf ;
+    from DFProstateChallenge.metadata rf group by rf.mrn;
     select CONCAT('Processed/', rf.mrn, '/KTRANS.raw.nii.gz:\n\tmkdir -p $(@D); c3d /rsrch1/ip/dtfuentes/PROSTATExChallenge2/KtransTrain/'   , rf.mrn,'/', rf.KTRANSUID , ' -o $@' ) 
-    from DFProstateChallenge.metadata rf ;
-    select CONCAT('Processed/', rf.mrn, '/landmarks.txt:\n\tmkdir -p $(@D); echo '   , rf.pos,' 1  > $@' ) 
-    from DFProstateChallenge.metadata rf ;
+    from DFProstateChallenge.metadata rf group by rf.mrn;
+    select CONCAT('Processed/', rf.mrn, '/landmarks.',rf.fid,'.txt:\n\tmkdir -p $(@D); echo '   , rf.pos         ,' ', rf.ggg,'  > $@' ) from DFProstateChallenge.metadata rf ;
+    select CONCAT('Processed/', rf.mrn, '/T2Axial.world:\n\tmkdir -p $(@D); echo '   , rf.T2AxialWorld,            '  > $@' ) from DFProstateChallenge.metadata rf group by rf.mrn;
+    select CONCAT('Processed/', rf.mrn, '/T2Sag.world:\n\tmkdir   -p $(@D); echo '   , rf.T2SagWorld  ,            '  > $@' ) from DFProstateChallenge.metadata rf group by rf.mrn;
+    select CONCAT('Processed/', rf.mrn, '/ADC.world:\n\tmkdir     -p $(@D); echo '   , rf.ADCWorld    ,            '  > $@' ) from DFProstateChallenge.metadata rf group by rf.mrn;
+    select CONCAT('Processed/', rf.mrn, '/BVAL.world:\n\tmkdir    -p $(@D); echo '   , rf.BVALWorld   ,            '  > $@' ) from DFProstateChallenge.metadata rf group by rf.mrn;
+    select CONCAT('Processed/', rf.mrn, '/T2Axial.',rf.fid,'.ijk:\n\tmkdir   -p $(@D); echo '   , rf.T2AxialIJK  ,' ', rf.ggg,'  > $@' ) from DFProstateChallenge.metadata rf ;
+    select CONCAT('Processed/', rf.mrn, '/T2Sag.',rf.fid,'.ijk:\n\tmkdir     -p $(@D); echo '   , rf.T2SagIJK    ,' ', rf.ggg,'  > $@' ) from DFProstateChallenge.metadata rf ;
+    select CONCAT('Processed/', rf.mrn, '/ADC.',rf.fid,'.ijk:\n\tmkdir       -p $(@D); echo '   , rf.ADCIJK      ,' ', rf.ggg,'  > $@' ) from DFProstateChallenge.metadata rf ;
+    select CONCAT('Processed/', rf.mrn, '/BVAL.',rf.fid,'.ijk:\n\tmkdir      -p $(@D); echo '   , rf.BVALIJK     ,' ', rf.ggg,'  > $@' ) from DFProstateChallenge.metadata rf ;
 
 END //
 DELIMITER ;
