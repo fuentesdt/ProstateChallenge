2e6203855b44a226ace0e70820e10b1ea14889ed
gpauloski <gpauloski@yahoo.com>
2017-07-17 12:03:03 -0500

    Fixed typo that made prediction seem very accurate

--- runRF_v1.R
+++ runRF_v1.R
@@ -5,7 +5,7 @@ dataset <- read.csv("file:///home/gpauloski/Downloads/PC/truthadcdatamatrix.csv"
 # Model Parameters
 
 seed <- 42		# RF seed
-partition <- 0.6	# % of data to be used in test set
+partition <- 0.7	# % of data to be used in test set
 trees <- 200		# Num trees for RF
 trys <- 2		# Num variables for RF
 split <- 4		# if 4; Z = 1 for ggg = 1,2,3
@@ -42,21 +42,21 @@ print(rf)
 
 # Apply RF Model on testing data to predict Z
 
-Z2 <- as.numeric(predict(rf, dataset[test,c(input,target)], type="response"))
+Z2 <- as.numeric(predict(rf, dataset[,c(input,target)], type="response"))
 cat("\nPredicting Z for test set using RF model\n")
 
 # Create new dataset with test data and predicted Z
 
-cat("\nBuilding new data set with Z predictions\n")
-dataset2 <- cbind(dataset[test,], Z2)
+cat("\nAdding Z predictions to dataset\n")
+dataset <- cbind(dataset, Z2)
 input2 <- c(input, "Z2")
 target2 <- "ggg"
-dataset2$ggg <- as.factor(dataset2$ggg)
+dataset$ggg <- as.factor(dataset$ggg)
 
 # Build and Apply RF Model on new dataset to predict ggg
 
-rf2 <- randomForest::randomForest(dataset2$ggg~., 
-	data=dataset2[,cbind(input2,target2)], 
+rf2 <- randomForest::randomForest(dataset$ggg[test]~., 
+	data=dataset[test,c(input2,target2)],#using cbind instead of c gives better results for some reason 
 	ntree=trees, 
 	mtry=trys)
 print(rf2)
